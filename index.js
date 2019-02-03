64d0adb74342fb4acf167663b36287a06d303220
Niya Panamdanam <niya@findniya.com>
2019-02-03 16:05:36 -0500

    WIP Initial setup for aws rekognition

--- index.js
+++ index.js
@@ -22,11 +22,22 @@ const upload = multer({
     s3: newS3,
     bucket: process.env.BUCKET_NAME,
     acl: 'public-read',
+    metadata(req, file, cb) {
+      cb(null, {fieldName: file.fieldname});
+    },
     key (req, file, cb) {
       cb(null, Date.now().toString() + '.jpg')
     }
   })
 })
+
+//AWS Rekognition
+const rekognition = new aws.Rekognition({
+  accessKeyId: process.env.KEY_ID,
+  secretAccessKey: process.env.SECRET_KEY,
+  region: 'us-east-1'
+})
+
 
 const express = require('express')
 const app = express()
@@ -38,6 +49,30 @@ app.get('/', (req, res) => {
 
 app.post('/upload', upload.single('photo'), (req, res, next) => {
   res.json(req.file)
+})
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.post('/rekognition', (req, res, next) => {
+  const params = {
+    Image: {
+      S3Object: {
+        Bucket: process.env.BUCKET_NAME,
+        Name: req.body.key
+      }
+    },
+    MaxLabels: 2,
+    MinConfidence: 95,
+  }
+  rekognition.detectLabels(params,((err, data) => {
+    if (err) {
+      console.log(err, err.stack)
+    }
+    else {
+      console.log(data)
+    }
+  }))
 })
 
 app.listen(port, (err) => {
